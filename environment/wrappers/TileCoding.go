39118e1d8717f084b8baabe4ff4730c0dc025a01
Samuel Neumann <samuelfneumann@gmail.com>
2021-05-18 13:00:37 -0600

    Add TileCoder
    Started adding a tile coder to the environment/wrappers package. This
    tile coder will be used to create a TileCodedEnvironment wrapper. The
    tile coder works with both vectors and batchs of samples as mat.Dense.
    
    The implementation has not been fully tested yet.

---
+++ environment/wrappers/TileCoding.go
@@ -0,0 +1,157 @@
+// Package wrappers provides wrappers for environments
+package wrappers
+
+import (
+	"math"
+
+	"golang.org/x/exp/rand"
+
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/spatial/r1"
+	"gonum.org/v1/gonum/stat/distmv"
+	"gonum.org/v1/gonum/stat/samplemv"
+)
+
+const OffsetDiv float64 = 10000.0
+
+// For now, we just implement a plain tile coder
+
+type TileCoder struct {
+	numTilings   int
+	minDims      mat.Vector
+	offsets      []*mat.Dense
+	bins         []int
+	binLengths   []float64
+	seed         uint64
+	tilingLength int
+}
+
+func NewTileCoder(numTilings int, minDims, maxDims mat.Vector, bins []int,
+	seed uint64) TileCoder {
+
+	var bounds []r1.Interval
+	binLengths := make([]float64, len(bins))
+	for i := 0; i < minDims.Len(); i++ {
+		// Calculate the length of bins
+		binLength := (maxDims.AtVec(i) - minDims.AtVec(i)) / float64(bins[i])
+		bound := binLength / OffsetDiv
+
+		binLengths[i] = binLength
+		bounds = append(bounds, r1.Interval{Min: -bound, Max: bound})
+	}
+
+	// Calculate offsets
+	var offsets []*mat.Dense
+	source := rand.NewSource(seed)
+	u := distmv.NewUniform(bounds, source)
+	sampler := samplemv.IID{Dist: u}
+	for i := 0; i < numTilings; i++ {
+		samples := mat.NewDense(1, len(bounds), nil)
+		sampler.Sample(samples)
+
+		offsets = append(offsets, samples)
+	}
+
+	tilingLength := prod(bins)
+
+	return TileCoder{numTilings, minDims, offsets, bins, binLengths, seed, tilingLength}
+}
+
+func (t TileCoder) EncodeBatch(b *mat.Dense) *mat.Dense {
+	rows, cols := b.Dims()
+	tileCoded := mat.NewDense(rows, t.VecLength(), nil)
+
+	// Create vector of ones
+	oneSlice := make([]float64, cols)
+	for i := 0; i < cols; i++ {
+		oneSlice[i] = 1.0
+	}
+	ones := mat.NewVecDense(cols, oneSlice)
+
+	for j := 0; j < t.numTilings; j++ {
+		indexOffset := j * t.numTilings
+		index := mat.NewVecDense(cols, nil)
+
+		for i := len(t.bins) - 1; i > -1; i-- {
+			data := b.ColView(i).(*mat.VecDense)
+			data.AddScaledVec(data, t.offsets[j].At(0, i), ones)
+			data.AddScaledVec(data, -t.minDims.AtVec(i), ones)
+
+			VecFloor(data, t.binLengths[i])
+			VecClip(data, 0.0, float64(t.bins[i]-1))
+
+			if i == len(t.bins)-1 {
+				index.AddVec(index, data)
+			} else {
+				index.AddScaledVec(index, float64(t.bins[i+1]), data)
+			}
+		}
+
+		// Set the proper 1.0 values
+		for i := 0; i < index.Len(); i++ {
+			col := indexOffset + int(index.AtVec(i))
+			tileCoded.Set(i, col, 1.0)
+		}
+	}
+	return tileCoded
+}
+
+func VecClip(a *mat.VecDense, min, max float64) {
+	for i := 0; i < a.Len(); i++ {
+		value := a.AtVec(i)
+
+		if value < min {
+			a.SetVec(i, min)
+		} else if value > max {
+			a.SetVec(i, max)
+		}
+	}
+}
+
+func VecFloor(a *mat.VecDense, b float64) {
+	for i := 0; i < a.Len(); i++ {
+		mod := math.Floor(a.AtVec(i) / b)
+		a.SetVec(i, mod)
+	}
+}
+
+func (t TileCoder) Encode(v mat.Vector) *mat.VecDense {
+	tileCoded := mat.NewVecDense(t.VecLength(), nil)
+
+	for j := 0; j < t.numTilings; j++ {
+		indexOffset := j * t.tilingLength
+		index := 0
+
+		for i := len(t.bins) - 1; i > -1; i-- {
+			// Offset the tiling
+			data := v.AtVec(i) + t.offsets[j].At(0, i)
+
+			tile := math.Floor((data - t.minDims.AtVec(i)) / t.binLengths[i])
+
+			// Clip tile to within tiling bounds
+			tile = math.Min(tile, float64(t.bins[i]-1))
+			tile = math.Max(tile, 0)
+
+			tileIndex := int(tile)
+			if i == len(t.bins)-1 {
+				index += tileIndex
+			} else {
+				index += tileIndex * t.bins[i+1]
+			}
+		}
+		tileCoded.SetVec(indexOffset+index, 1.0)
+	}
+	return tileCoded
+}
+
+func (t TileCoder) VecLength() int {
+	return t.numTilings * t.tilingLength
+}
+
+func prod(i []int) int {
+	prod := 1
+	for _, v := range i {
+		prod *= v
+	}
+	return prod
+}
--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 
 	"gonum.org/v1/gonum/mat"
-	"sfneuman.com/golearn/agent/linear/discrete/qlearning"
-	"sfneuman.com/golearn/environment/gridworld"
-	"sfneuman.com/golearn/spec"
-	"sfneuman.com/golearn/utils/matutils/initializers/weights"
+	"sfneuman.com/golearn/environment/wrappers"
+	"sfneuman.com/golearn/utils/matutils"
 )
 
 // "golang.org/x/exp/rand"
@@ -15,115 +13,115 @@ import (
 // "sfneuman.com/golearn/utils/matutils"
 
 func main() {
-	var seed uint64 = 192312
-	// === === === === === === === === === === === === === === === ===
-	// GridWorld
-	r, c := 5, 5
+	var seed uint64 = 19232
+	// // === === === === === === === === === === === === === === === ===
+	// // GridWorld
+	// r, c := 5, 5
 
-	// Create the start-state distribution
-	starter, err := gridworld.NewSingleStart(0, 0, r, c)
-	if err != nil {
-		fmt.Println("Could not create starter")
-		return
-	}
+	// // Create the start-state distribution
+	// starter, err := gridworld.NewSingleStart(0, 0, r, c)
+	// if err != nil {
+	// 	fmt.Println("Could not create starter")
+	// 	return
+	// }
 
-	// Create the gridworld task
-	x := []int{4}
-	y := []int{4}
-	goal, err := gridworld.NewGoal(starter, x, y, r, c, -0.1, 1.0)
-	if err != nil {
-		fmt.Println("Could not create goal")
-		return
-	}
+	// // Create the gridworld task
+	// x := []int{4}
+	// y := []int{4}
+	// goal, err := gridworld.NewGoal(starter, x, y, r, c, -0.1, 1.0)
+	// if err != nil {
+	// 	fmt.Println("Could not create goal")
+	// 	return
+	// }
 
-	// Create the gridworld
-	g, t := gridworld.New(r, c, goal, 0.99)
-	fmt.Println(t)
-	fmt.Println(g)
+	// // Create the gridworld
+	// g, t := gridworld.New(r, c, goal, 0.99)
+	// fmt.Println(t)
+	// fmt.Println(g)
 
-	// Create the QLearning spec
-	args := spec.QLearning{E: 0.1, LearningRate: 0.5}
-	// args := spec.ESarsa{TargetE: 0.0, BehaviourE: 0.1, LearningRate: 0.1}
+	// // Create the QLearning spec
+	// args := spec.QLearning{E: 0.1, LearningRate: 0.5}
+	// // args := spec.ESarsa{TargetE: 0.0, BehaviourE: 0.1, LearningRate: 0.1}
 
-	// Zero RNG
-	weightSize := make([]float64, r*c)
-	rand := weights.NewZero(weightSize)
+	// // Zero RNG
+	// weightSize := make([]float64, r*c)
+	// rand := weights.NewZero(weightSize)
 
-	// // Normal RNG
-	// source := rand.NewSource(seed)
-	// var mean []float64
-	// var std []float64
-	// for i := 0; i < c*r; i++ {
-	// 	mean = append(mean, 0.2)
-	// 	for j := 0; j < c*r; j++ {
-	// 		if i != j {
-	// 			std = append(std, 0.0)
-	// 		} else {
-	// 			std = append(std, 0.0001)
-	// 		}
+	// // // Normal RNG
+	// // source := rand.NewSource(seed)
+	// // var mean []float64
+	// // var std []float64
+	// // for i := 0; i < c*r; i++ {
+	// // 	mean = append(mean, 0.2)
+	// // 	for j := 0; j < c*r; j++ {
+	// // 		if i != j {
+	// // 			std = append(std, 0.0)
+	// // 		} else {
+	// // 			std = append(std, 0.0001)
+	// // 		}
+	// // 	}
+	// // }
+	// // stddev := mat.NewSymDense(c*r, std)
+	// // rand, ok := distmv.NewNormal(mean, stddev, source)
+	// // if !ok {
+	// // 	panic("could not create distribution")
+	// // }
+
+	// // Create the weight initializer with the RNG
+	// init := weights.NewLinear(rand)
+
+	// // Create the learning algorithm
+	// q := qlearning.New(g, args, init, seed)
+	// // q := esarsa.New(g, args, init, seed)
+	// q.ObserveFirst(t)
+
+	// // Track the return
+	// total := 0.0
+	// episodicReward := make([]float64, 100)
+	// episodeReward := 0.0
+
+	// for i := 0; i < 500000; i++ {
+	// 	// Take an action and send to env
+	// 	action := q.SelectAction(t)
+	// 	t, _ = g.Step(action)
+
+	// 	// Track return
+	// 	total += t.Reward
+	// 	episodeReward += t.Reward
+
+	// 	// Observe environmental change
+	// 	q.Observe(action, t)
+	// 	q.Step()
+
+	// 	// Reset env if end of episode
+	// 	if t.Last() {
+	// 		episodicReward = append(episodicReward, episodeReward)
+	// 		episodeReward = 0.0
+
+	// 		// Reset the environment and observe the first episode transition
+	// 		t = g.Reset()
+	// 		q.ObserveFirst(t)
 	// 	}
 	// }
-	// stddev := mat.NewSymDense(c*r, std)
-	// rand, ok := distmv.NewNormal(mean, stddev, source)
-	// if !ok {
-	// 	panic("could not create distribution")
+
+	// fmt.Println()
+	// actions := []string{"left", "right", "up", "down"}
+	// w := q.Learner.Weights()["weights"]
+	// r, _ = w.Dims()
+	// for i := 0; i < r; i++ {
+	// 	fmt.Println("=== Action:", actions[i], "===")
+	// 	fmt.Println(w.RowView(i))
+	// 	fmt.Println()
 	// }
+	// w1 := q.Policy.Weights()["weights"]
+	// w2 := q.Target.Weights()["weights"]
 
-	// Create the weight initializer with the RNG
-	init := weights.NewLinear(rand)
+	// fmt.Println(mat.Equal(w1, w))
+	// fmt.Println(mat.Equal(w, w2))
+	// fmt.Println(mat.Equal(w1, w2))
 
-	// Create the learning algorithm
-	q := qlearning.New(g, args, init, seed)
-	// q := esarsa.New(g, args, init, seed)
-	q.ObserveFirst(t)
-
-	// Track the return
-	total := 0.0
-	episodicReward := make([]float64, 100)
-	episodeReward := 0.0
-
-	for i := 0; i < 500000; i++ {
-		// Take an action and send to env
-		action := q.SelectAction(t)
-		t, _ = g.Step(action)
-
-		// Track return
-		total += t.Reward
-		episodeReward += t.Reward
-
-		// Observe environmental change
-		q.Observe(action, t)
-		q.Step()
-
-		// Reset env if end of episode
-		if t.Last() {
-			episodicReward = append(episodicReward, episodeReward)
-			episodeReward = 0.0
-
-			// Reset the environment and observe the first episode transition
-			t = g.Reset()
-			q.ObserveFirst(t)
-		}
-	}
-
-	fmt.Println()
-	actions := []string{"left", "right", "up", "down"}
-	w := q.Learner.Weights()["weights"]
-	r, _ = w.Dims()
-	for i := 0; i < r; i++ {
-		fmt.Println("=== Action:", actions[i], "===")
-		fmt.Println(w.RowView(i))
-		fmt.Println()
-	}
-	w1 := q.Policy.Weights()["weights"]
-	w2 := q.Target.Weights()["weights"]
-
-	fmt.Println(mat.Equal(w1, w))
-	fmt.Println(mat.Equal(w, w2))
-	fmt.Println(mat.Equal(w1, w2))
-
-	length := len(episodicReward)
-	fmt.Println(episodicReward[length-5 : length])
+	// length := len(episodicReward)
+	// fmt.Println(episodicReward[length-5 : length])
 
 	// // === === === === === === === === === === === === === === === ===
 	// // Pendulum
@@ -181,4 +179,21 @@ func main() {
 	// 		t = m.Reset()
 	// 	}
 	// }
+
+	// === === === === === === === === === === === === === === === ===
+	// Tile Coding
+	minDims := mat.NewVecDense(2, []float64{-2.0, -4})
+	maxDims := mat.NewVecDense(2, []float64{2.0, 4})
+	bins := []int{2, 8}
+
+	v := mat.NewVecDense(2, []float64{-0.1, -30.1})
+	v2 := mat.NewVecDense(2, []float64{0.0, 0.0})
+
+	t := wrappers.NewTileCoder(1, minDims, maxDims, bins, seed)
+
+	b := mat.NewDense(2, 2, []float64{-0.1, -30.1, 0.0, 0.0})
+
+	fmt.Println(matutils.Format(t.Encode(v).T()))
+	fmt.Println(matutils.Format(t.Encode(v2).T()))
+	fmt.Println(matutils.Format(t.EncodeBatch(b)))
 }
