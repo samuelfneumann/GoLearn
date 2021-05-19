bdfe664ebde73e226f60ce29aafa0d15cca2b1b8
Samuel Neumann <samuelfneumann@gmail.com>
2021-05-19 06:31:20 -0600

    Transpose batch
    Changed the functionality of the tile coder. When tile coding batches,
    we now assume that each row is a feature and that columns are samples in
    the batch. For example, if given a matrix of shape 3 x 10, this function
    assumes that we have a batch of 10 samples, where each samples is 3
    features.
    
    This was done to improve performance. Since Go uses row-major order,
    this change will result in the tile coder requiring to read a contiguous
    region of memory to access a single feature among all samples in the
    batch in order to tile-code. Before, each feature was a column. When
    accessing a feature for each sample in the batch, this may have caused
    Go to read multiple discontiguous regions of memory, which would be more
    expensive.
    
    When tile coding a batch, we access (as a vector) a single feature for
    all samples in the batch, and calculate the tile-coded index based on
    calculations of these features. Because Go uses row-major order and we
    now set the rows of a matrix as the features, when selecting a feature
    among all samples in the batch, Go will require a read to a contiguous
    region of memory. If features were columns, Go would require reading
    multiple discontiguous regions of memory in order to access this feature
    among all samples.

--- environment/wrappers/TileCoding.go
+++ environment/wrappers/TileCoding.go
@@ -10,41 +10,73 @@ import (
 	"gonum.org/v1/gonum/spatial/r1"
 	"gonum.org/v1/gonum/stat/distmv"
 	"gonum.org/v1/gonum/stat/samplemv"
+
+	"sfneuman.com/golearn/utils/matutils"
 )
 
+// Controls tiling offsets. For each dimension, tilings are offset by
+// randomly sampling from a uniform distribution with support
+// [- tiling width/OffsetDiv, tiling width/OffsetDiv]
 const OffsetDiv float64 = 1.5
 
-// For now, we just implement a plain tile coder
-
+// TileCoder implements functionality for tile coding a vector. Tile
+// coding takes a low-dimensional vector and changes it into a large,
+// sparse vector consisting of only 0's and 1's. Each 1 represents the
+// coordinates of the original vector in some space of tilings. For
+// example:
+//
+//		[0.5, 0.1] -> [0, 0, 0, 1, 0, 0, 1, 0]
+//
+//
+// The number of nonzero elements in the tile-coded representation equals
+// the number of tilings used to encode the vector. The number of total
+// features in the tile-coded representation is the number of tilings
+// times the number of tiles per tiling. Tile coding requires that the
+// space to be tiled be bounded.
+//
+// This implementation of tile coding uses dense tilings over the entire
+// state space. That is, each dimension of state space is fully tiled,
+// and hash-based tile coding is not used. This implementation also
+// uses multiple tilings, each of which consist of the name number
+// of tiles per tiling.
 type TileCoder struct {
-	numTilings   int
-	minDims      mat.Vector
-	offsets      []*mat.Dense
-	bins         []int
-	binLengths   []float64
-	seed         uint64
-	tilingLength int
+	numTilings        int
+	minDims           mat.Vector
+	offsets           []*mat.Dense
+	bins              []int
+	binLengths        []float64
+	seed              uint64
+	featuresPerTiling int
 }
 
+// NewTileCoder creates and returns a new TileCoder struct. The minDims
+// and maxDims arguments are the bounds on each dimension between which
+// tilings will be placed. These arguments should have the same shape
+// as vectors which will be tile coded. The bins argument determines
+// how many tiles are placed (per tilings) along each dimension and
+// should have the same number of elements as the minDims and maxDims
+// arguments.
 func NewTileCoder(numTilings int, minDims, maxDims mat.Vector, bins []int,
 	seed uint64) TileCoder {
-
+	// Calculate the length of bins and the tiling offset bounds
 	var bounds []r1.Interval
 	binLengths := make([]float64, len(bins))
 	for i := 0; i < minDims.Len(); i++ {
 		// Calculate the length of bins
 		binLength := (maxDims.AtVec(i) - minDims.AtVec(i)) / float64(bins[i])
-		bound := binLength / OffsetDiv
+		bound := binLength / OffsetDiv // Bounds tiling offsets
 
 		binLengths[i] = binLength
 		bounds = append(bounds, r1.Interval{Min: -bound, Max: bound})
 	}
 
-	// Calculate offsets
-	var offsets []*mat.Dense
+	// Create RNG for uniform sampling of tiling offsets
 	source := rand.NewSource(seed)
 	u := distmv.NewUniform(bounds, source)
 	sampler := samplemv.IID{Dist: u}
+
+	// Calculate offsets
+	var offsets []*mat.Dense
 	for i := 0; i < numTilings; i++ {
 		samples := mat.NewDense(1, len(bounds), nil)
 		sampler.Sample(samples)
@@ -52,39 +84,41 @@ func NewTileCoder(numTilings int, minDims, maxDims mat.Vector, bins []int,
 		offsets = append(offsets, samples)
 	}
 
-	tilingLength := prod(bins)
+	// Number of features in each tiling
+	featuresPerTiling := prod(bins)
 
-	return TileCoder{numTilings, minDims, offsets, bins, binLengths, seed, tilingLength}
+	return TileCoder{numTilings, minDims, offsets, bins, binLengths, seed,
+		featuresPerTiling}
 }
 
 func (t TileCoder) EncodeBatch(b *mat.Dense) *mat.Dense {
 	rows, cols := b.Dims()
-	tileCoded := mat.NewDense(rows, t.VecLength(), nil)
+	tileCoded := mat.NewDense(t.VecLength(), cols, nil)
 
 	// Create vector of ones
 	oneSlice := make([]float64, cols)
-	for i := 0; i < cols; i++ {
+	for i := 0; i < rows; i++ {
 		oneSlice[i] = 1.0
 	}
-	ones := mat.NewVecDense(cols, oneSlice)
+	ones := mat.NewVecDense(rows, oneSlice)
 
 	// Vector that holds all the data that is manipulated
-	data := mat.NewVecDense(cols, nil)
+	data := mat.NewVecDense(rows, nil)
 
 	// Tile code
 	for j := 0; j < t.numTilings; j++ {
-		indexOffset := j * t.tilingLength
-		index := mat.NewVecDense(cols, nil)
+		indexOffset := j * t.featuresPerTiling
+		index := mat.NewVecDense(rows, nil)
 
 		for i := len(t.bins) - 1; i > -1; i-- {
 			// Clone the next batch of features into the data vector
-			data.CloneFromVec(b.ColView(i))
+			data.CloneFromVec(b.RowView(i))
 
 			data.AddScaledVec(data, t.offsets[j].At(0, i), ones)
 			data.AddScaledVec(data, -t.minDims.AtVec(i), ones)
 
-			VecFloor(data, t.binLengths[i])
-			VecClip(data, 0.0, float64(t.bins[i]-1))
+			matutils.VecFloor(data, t.binLengths[i])
+			matutils.VecClip(data, 0.0, float64(t.bins[i]-1))
 
 			if i == len(t.bins)-1 {
 				index.AddVec(index, data)
@@ -95,37 +129,18 @@ func (t TileCoder) EncodeBatch(b *mat.Dense) *mat.Dense {
 
 		// Set the proper 1.0 values
 		for i := 0; i < index.Len(); i++ {
-			col := indexOffset + int(index.AtVec(i))
-			tileCoded.Set(i, col, 1.0)
+			row := indexOffset + int(index.AtVec(i))
+			tileCoded.Set(row, i, 1.0)
 		}
 	}
 	return tileCoded
-}
-
-func VecClip(a *mat.VecDense, min, max float64) {
-	for i := 0; i < a.Len(); i++ {
-		value := a.AtVec(i)
-
-		if value < min {
-			a.SetVec(i, min)
-		} else if value > max {
-			a.SetVec(i, max)
-		}
-	}
-}
-
-func VecFloor(a *mat.VecDense, b float64) {
-	for i := 0; i < a.Len(); i++ {
-		mod := math.Floor(a.AtVec(i) / b)
-		a.SetVec(i, mod)
-	}
 }
 
 func (t TileCoder) Encode(v mat.Vector) *mat.VecDense {
 	tileCoded := mat.NewVecDense(t.VecLength(), nil)
 
 	for j := 0; j < t.numTilings; j++ {
-		indexOffset := j * t.tilingLength
+		indexOffset := j * t.featuresPerTiling
 		index := 0
 
 		for i := len(t.bins) - 1; i > -1; i-- {
@@ -151,7 +166,7 @@ func (t TileCoder) Encode(v mat.Vector) *mat.VecDense {
 }
 
 func (t TileCoder) VecLength() int {
-	return t.numTilings * t.tilingLength
+	return t.numTilings * t.featuresPerTiling
 }
 
 func prod(i []int) int {
