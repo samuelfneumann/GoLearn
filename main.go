e0e849001dc11726f3caecb630af97e24d308d13
Samuel Neumann <samuelfneumann@gmail.com>
2021-06-23 16:02:58 -0600

    Test in main

--- main.go
+++ main.go
@@ -2,66 +2,52 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"gonum.org/v1/gonum/spatial/r1"
-	"sfneuman.com/golearn/agent/linear/continuous/actorcritic"
+	"gorgonia.org/gorgonia"
+	"sfneuman.com/golearn/agent/nonlinear/discrete/deepq"
+	"sfneuman.com/golearn/agent/nonlinear/discrete/policy"
 	"sfneuman.com/golearn/environment"
 	"sfneuman.com/golearn/environment/classiccontrol/mountaincar"
-	"sfneuman.com/golearn/environment/wrappers"
 	"sfneuman.com/golearn/experiment"
 	"sfneuman.com/golearn/experiment/trackers"
-	"sfneuman.com/golearn/spec"
-	"sfneuman.com/golearn/utils/matutils/initializers/weights"
 )
 
 func main() {
-	var seed uint64 = 192382
+	var useed uint64 = 192382
+	var seed int64 = 192382
 
 	// Create the environment
 	bounds := r1.Interval{Min: -0.01, Max: 0.01}
 
-	s := environment.NewUniformStarter([]r1.Interval{bounds, bounds}, seed)
-	task := mountaincar.NewGoal(s, 1000, mountaincar.GoalPosition)
-	m, _ := mountaincar.NewContinuous(task, 1.0)
-
-	// Create the TileCoding env wrapper
-	numTilings := 10
-	tilings := make([][]int, numTilings)
-
-	for i := 0; i < len(tilings); i++ {
-		tilings[i] = []int{10, 10}
-	}
-	tm, _ := wrappers.NewTileCoding(m, tilings, seed)
-
-	ttm, _ := wrappers.NewAverageReward(tm, 0.0, 0.01, true)
-
-	// Zero RNG
-	rand := weights.NewZeroUV()
-
-	// Create the weight initializer with the RNG
-	init := weights.NewLinearUV(rand)
+	s := environment.NewUniformStarter([]r1.Interval{bounds, bounds}, useed)
+	task := mountaincar.NewGoal(s, 250, mountaincar.GoalPosition)
+	m, _ := mountaincar.NewDiscrete(task, 1.0)
 
 	// Create the learning algorithm
-	args := spec.LinearGaussianActorCritic{
-		ActorLearningRate:  0.001,
-		CriticLearningRate: 0.01,
-		Decay:              0.5,
+	args := deepq.Config{
+		PolicyLayers: []int{100, 50},
+		Biases:       []bool{true, true},
+		Activations:  []policy.Activation{gorgonia.Rectify, gorgonia.Rectify},
+		InitWFn:      gorgonia.GlorotU(1.0),
+		LearningRate: 0.0001,
+		epsilon:      0.1,
 	}
-	q, err := actorcritic.NewLinearGaussian(tm, args, init, seed)
+	q, err := deepq.New(m, args, seed)
 	if err != nil {
 		panic(err)
 	}
 
-	// Register learner with average reward
-	ttm.Register(q)
-
 	// Experiment
+	start := time.Now()
 	var tracker trackers.Tracker = trackers.NewReturn("./data.bin")
 	tracker = trackers.Register(tracker, m)
-	e := experiment.NewOnline(ttm, q, 1_000_000, tracker)
+	e := experiment.NewOnline(m, q, 20_000, tracker)
 	e.Run()
+	fmt.Println("Elapsed:", time.Since(start))
 	e.Save()
 
 	data := trackers.LoadData("./data.bin")
-	fmt.Println(data[len(data)-10:])
+	fmt.Println(data)
 }
