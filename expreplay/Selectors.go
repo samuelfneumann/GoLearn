75682ce0e1f6f740e6e90a9cbc6c58cab8540ae0
Samuel Neumann <samuelfneumann@gmail.com>
2021-06-25 16:15:16 -0600

    Add experience replay

---
+++ expreplay/Selectors.go
@@ -0,0 +1,107 @@
+package expreplay
+
+import (
+	"math/rand"
+
+	"sfneuman.com/golearn/utils/intutils"
+)
+
+// Selector implements functionality for choosing how data should be
+// sampled and/or removed from an experience replay buffer
+type Selector interface {
+	// choose selects the indices at which data should be sampled from
+	// the experience replay buffer
+	choose(c *cache) []int
+
+	// size returns the number of elements that will be sampled from or
+	// removed from the buffer
+	size() int
+
+	// registerAsRemover registers a Selector as a remover
+	//
+	//Some Selectors require different behaviour if they are removers,
+	// so they should be notified if they become a remover to add this
+	// additional behaviour
+	registerAsRemover()
+}
+
+// uniformSelector is a Selector which selects data from an experience
+// replay buffer uniformly randomly
+type uniformSelector struct {
+	samples int
+	rng     *rand.Rand
+}
+
+// NewUniformSelector returns a new Selector which selects data uniformly
+// randomly from an experience replay buffer
+func NewUniformSelector(samples int, seed int64) Selector {
+	source := rand.NewSource(seed)
+	rng := rand.New(source)
+
+	return &uniformSelector{samples: samples, rng: rng}
+}
+
+// registerAsRemover implements Selector interface
+func (u *uniformSelector) registerAsRemover() {}
+
+// size gets the number of samples in a batch drawn from the buffer
+func (u *uniformSelector) size() int {
+	return u.samples
+}
+
+// choose selects a number of indices at which to draw data from the
+// buffer
+func (u *uniformSelector) choose(c *cache) []int {
+	selected := make([]int, u.size())
+	keys := keysWithValue(c.emptyIndices, false)
+
+	for i := 0; i < u.size(); i++ {
+		index := u.rng.Int() % c.Capacity()
+		selected[i] = keys[index]
+	}
+
+	return selected
+}
+
+// fifoSelector is a Selector which selects data from an experience
+// replay buffer as first-in-first-out.
+type fifoSelector struct {
+	samples int
+	remover bool
+}
+
+// NewFifoSelector returns a new Selector which draws data from an
+// experience replay buffer in as FiFo.
+func NewFifoSelector(samples int) Selector {
+	return &fifoSelector{samples: samples, remover: false}
+}
+
+// registerAsRemover implements Selector interface
+func (f *fifoSelector) registerAsRemover() {
+	f.remover = true
+}
+
+// size gets the number of samples in a batch drawn from the buffer
+func (f *fifoSelector) size() int {
+	return f.samples
+}
+
+// choose selects a number of indices at which to draw data from the
+// buffer
+func (f *fifoSelector) choose(c *cache) []int {
+	selected := make([]int, intutils.Min(f.size(), c.Capacity()))
+	insertOrder := c.insertOrder(f.size())
+
+	for i := 0; i < f.size() && i < c.Capacity(); i++ {
+		selected[i] = insertOrder[i]
+
+		if f.remover {
+			// In a Fifo remover, the indices at which data was first
+			// added get freed first, so we can remove these from the
+			// ordering of inserted indices
+			c.removeFront()
+		}
+	}
+
+	return selected
+}
---
+++ expreplay/expreplay.go
@@ -0,0 +1,303 @@
+package expreplay
+
+import (
+	"container/list"
+	"fmt"
+
+	"sfneuman.com/golearn/timestep"
+	"sfneuman.com/golearn/utils/intutils"
+)
+
+// ExperienceReplayer implements an experience replay buffer
+type ExperienceReplayer interface {
+	// Add adds a transition to the buffer
+	Add(t timestep.Transition) error
+
+	// Sample samples a batch of experience from the buffer and returns
+	// the batch of SARSA tuples as []float64
+	Sample() ([]float64, []float64, []float64, []float64, []float64,
+		[]float64, error)
+
+	// Capacity returns the current number of samples in the buffer
+	Capacity() int
+
+	// MaxCapacity returns the maximum allowable samples in the buffer
+	MaxCapacity() int
+
+	// MinCapacity returns the number of samples required to be in
+	// the buffer before the buffer can be sampled
+	MinCapacity() int
+
+	// BatchSize returns the number of samples returned by Sample()
+	BatchSize() int
+}
+
+// cache implements a concrete ExperienceReplayer
+type cache struct {
+	stateCache      []float64
+	actionCache     []float64
+	rewardCache     []float64
+	discountCache   []float64
+	nextStateCache  []float64
+	nextActionCache []float64
+
+	// Set of indices of experience that have data available for
+	// setting. True indicates that no data exists at the respective
+	// index and that data can be stored in the buffer at that index.
+	// False indicates that legal data exists at that index, and that
+	// the data at that index can be sampled from.
+	emptyIndices map[int]bool
+
+	// orderOfInsert outlines the order the chronological order of
+	// inserts. For i > j, the data at index orderOfInsert[i] was
+	// inserted into the buffer after the data at index orderOfInsert[j]
+	orderOfInsert *list.List
+
+	// Outlines how data is removed and sampled
+	remover Selector
+	sampler Selector
+
+	minCapacity int
+	maxCapacity int
+	featureSize int
+	actionSize  int
+}
+
+// New creates and returns a new ExperienceReplayer. The remover and
+// sampler paramters are Selectors which determine how data is removed
+// and sampled from the replay buffer. The featureSize and actionSize
+// parameters define the size of the feature and action vectors.
+//
+// Pixel observations should be flattened before adding to the buffer.
+func New(remover, sampler Selector, minCapacity, maxCapacity, featureSize,
+	actionSize int) (ExperienceReplayer, error) {
+	if minCapacity < 0 {
+		return &cache{}, fmt.Errorf("new: minCapacity must be >= 0")
+	}
+	if maxCapacity < 1 {
+		return &cache{}, fmt.Errorf("new: maxCapacity must be >= 1")
+	}
+
+	stateCache := make([]float64, maxCapacity*featureSize)
+	nextStateCache := make([]float64, maxCapacity*featureSize)
+
+	actionCache := make([]float64, maxCapacity*actionSize)
+	nextActionCache := make([]float64, maxCapacity*actionSize)
+
+	rewardCache := make([]float64, maxCapacity)
+	discountCache := make([]float64, maxCapacity)
+
+	// Create the set of indices at which data can be stored
+	emptyIndices := make(map[int]bool)
+	for i := 0; i < maxCapacity; i++ {
+		emptyIndices[i] = true
+	}
+	orderOfInsert := list.New()
+
+	remover.registerAsRemover()
+
+	return &cache{stateCache, actionCache, rewardCache, discountCache, nextStateCache,
+		nextActionCache, emptyIndices, orderOfInsert, remover, sampler, minCapacity,
+		maxCapacity, featureSize, actionSize}, nil
+}
+
+// insertOrder returns a slice of at most n indices which describes
+// the order that the first n data were inserted into the buffer.
+// The length of the returned slice is the minimum between n and the
+// number of elements currently in the buffer
+//
+// For example, if this function returns []int{9, 15, 1}, this means
+// that the first data was inserted into the buffer at position 9, the
+// next at position 15, and the last at position 1
+func (c *cache) insertOrder(n int) []int {
+	size := intutils.Min(n, c.Capacity())
+	insertOrder := make([]int, size)
+	element := c.orderOfInsert.Front()
+
+	for i := 0; i < size; i++ {
+		insertOrder[i] = element.Value.(int)
+		element = element.Next()
+		if element == nil {
+			break
+		}
+	}
+	return insertOrder
+}
+
+// String returns the string representation of the cache
+func (c *cache) String() string {
+	emptyIndices := keysWithValue(c.emptyIndices, true)
+	usedIndices := keysWithValue(c.emptyIndices, false)
+
+	baseStr := "Indices Available: %v \nIndices Used: %v \nStates: %v" +
+		" \nActions: %v \nRewards: %v \nDiscounts: %v \nNext States: %v \n" +
+		"Next Actions: %v"
+	return fmt.Sprintf(baseStr, emptyIndices, usedIndices, c.stateCache,
+		c.actionCache, c.rewardCache, c.discountCache, c.nextStateCache, c.nextActionCache)
+}
+
+// BatchSize returns the number of samples sampled using Sample() -
+// a.k.a the batch size
+func (c *cache) BatchSize() int {
+	return c.sampler.size()
+}
+
+// remove removes elements from the cache using indices sampled from the
+// cache's remover
+func (c *cache) remove() error {
+	if c.Capacity() <= c.minCapacity {
+		return fmt.Errorf("remove: cannot remove, cache at min capacity")
+	}
+
+	indices := c.remover.choose(c)
+	fmt.Println("REMOVING:", indices)
+	for _, index := range indices {
+		c.emptyIndices[index] = true
+	}
+	return nil
+}
+
+// removeFront removes the earliest index at which data was inserted at.
+//
+// The cache keeps track of the order of indices at which data was
+// inserted. This function will remove the earliest index from the front
+// of this list.
+func (c *cache) removeFront() {
+	c.orderOfInsert.Remove(c.orderOfInsert.Front())
+}
+
+// Sample samples and returns a batch of transitions from the replay
+// buffer
+func (c *cache) Sample() ([]float64, []float64, []float64, []float64,
+	[]float64, []float64, error) {
+	if c.Capacity() == 0 {
+		err := fmt.Errorf("sample: cannot sample from empty cache")
+		return nil, nil, nil, nil, nil, nil, err
+	}
+	if c.Capacity() < c.MinCapacity() {
+		err := fmt.Errorf("sample: cannot sample, minimum capacity " +
+			"not yet reached")
+		return nil, nil, nil, nil, nil, nil, err
+	}
+
+	indices := c.sampler.choose(c)
+
+	stateBatch := make([]float64, c.BatchSize()*c.featureSize)
+	nextStateBatch := make([]float64, c.BatchSize()*c.featureSize)
+	for i, index := range indices {
+		batchStartInd := i * c.featureSize
+		expStartInd := index * c.featureSize
+		copy(stateBatch[batchStartInd:batchStartInd+c.featureSize],
+			c.stateCache[expStartInd:expStartInd+c.featureSize],
+		)
+		copy(nextStateBatch[batchStartInd:batchStartInd+c.featureSize],
+			c.nextStateCache[expStartInd:expStartInd+c.featureSize],
+		)
+	}
+
+	actionBatch := make([]float64, c.BatchSize()*c.actionSize)
+	nextActionBatch := make([]float64, c.BatchSize()*c.actionSize)
+	for i, index := range indices {
+		batchStartInd := i * c.actionSize
+		expStartInd := index * c.actionSize
+		copy(actionBatch[batchStartInd:batchStartInd+c.actionSize],
+			c.actionCache[expStartInd:expStartInd+c.actionSize],
+		)
+		copy(nextActionBatch[batchStartInd:batchStartInd+c.actionSize],
+			c.nextActionCache[expStartInd:expStartInd+c.actionSize],
+		)
+	}
+
+	rewardBatch := make([]float64, c.BatchSize())
+	discountBatch := make([]float64, c.BatchSize())
+	for i, index := range indices {
+		discountBatch[i] = c.discountCache[index]
+		rewardBatch[i] = c.rewardCache[index]
+	}
+
+	return stateBatch, actionBatch, rewardBatch, discountBatch, nextStateBatch,
+		nextActionBatch, nil
+}
+
+// Capacity returns the current number of elements in the cache that
+// are available for sampling
+func (c *cache) Capacity() int {
+	total := 0
+	for _, value := range c.emptyIndices {
+		if value {
+			total++
+		}
+	}
+	return c.MaxCapacity() - total
+}
+
+// MaxCapacity returns the maximum number of elements that are allowed
+// in the cache
+func (c *cache) MaxCapacity() int {
+	return c.maxCapacity
+}
+
+// MinCapacity returns the minimum number of elements required in the
+// cache before sampling is allowed
+func (c *cache) MinCapacity() int {
+	return c.minCapacity
+}
+
+// Add adds a transition to the cache
+func (c *cache) Add(t timestep.Transition) error {
+	if c.Capacity() >= c.maxCapacity {
+		err := c.remove()
+		if err != nil {
+			return fmt.Errorf("add: cannot add to buffer: %v", err)
+		}
+	}
+
+	var index int
+	for index = range c.emptyIndices {
+		if c.emptyIndices[index] {
+			c.emptyIndices[index] = false
+			c.orderOfInsert.PushBack(index)
+			break
+		}
+	}
+
+	if t.State.Len() != c.featureSize || t.NextState.Len() != c.featureSize {
+		return fmt.Errorf("add: invalid feature size \n\twant(%v)\n\thave(%v)",
+			t.State.Len(), c.featureSize)
+	}
+	if t.Action.Len() != c.actionSize || t.NextAction.Len() != c.actionSize {
+		return fmt.Errorf("add: invalid action size \n\twant(%v)\n\thave(%v)",
+			t.State.Len(), c.featureSize)
+	}
+
+	// Copy states
+	stateInd := index * c.featureSize
+	copy(c.stateCache[stateInd:stateInd+c.featureSize], t.State.RawVector().Data)
+	copy(c.nextStateCache[stateInd:stateInd+c.featureSize],
+		t.NextState.RawVector().Data)
+
+	// Copy actions
+	actionInd := index * c.actionSize
+	copy(c.actionCache[actionInd:actionInd+c.actionSize],
+		t.Action.RawVector().Data)
+	copy(c.nextActionCache[actionInd:actionInd+c.actionSize],
+		t.Action.RawVector().Data)
+
+	// Copy reward R
+	c.rewardCache[index] = t.Reward
+	c.discountCache[index] = t.Discount
+
+	return nil
+}
+
+// keysWithValue gets all the keys in a set that have the corresponding
+// argument value
+func keysWithValue(set map[int]bool, value bool) []int {
+	keys := make([]int, 0, len(set))
+	for index, currentVal := range set {
+		if currentVal == value {
+			keys = append(keys, index)
+		}
+	}
+	return keys
+}
