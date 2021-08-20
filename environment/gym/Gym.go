4b506136e9a183672af7e9491c7a0a146dbc7339
Samuel Neumann <samuelfneumann@gmail.com>
2021-08-20 14:49:05 -0600

    Prepare gym package to use GoGym
    Removed all the code in the gym package. The gym package is going to now
    be implemented with [GoGym](https://github.com/samuelfneumann/GoGym),
    which provides Go bindings for OpenAI's Gym using cgo and the Python C
    API.
    
    By using GoGym, we shoud see significant performance improvements
    compared to using an HTTP server. Additionally, it is much easier to run
    on cloud computing resources, since we don't have to worry about getting
    access to a socket for the HTTP server (which may not even be possible
    on some cloud resources). Additionally, the implementation will no
    longer be restricted by the networking resources, and can easily be run
    concurrently.

---
+++ environment/gym/Gym.go
@@ -0,0 +1,6 @@
+// Package gym provides access to use OpenAI's Gym environments in
+// GoLearn.
+//
+// This is made possible through the Go bindings for OpenAI Gym,
+// found at https://github.com/samuelfneumann/GoGym.
+package gym
